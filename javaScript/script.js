8886d340fbe57ac109e414f4a744125bc29a6aea
Karthik B <karthikbalu5533@gmail.com>
2025-12-19 21:04:27 +0530

    added validations for text space

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,23 +1,29 @@
 const input = document.getElementById("taskInput")
 const button = document.getElementById("btnClick")
 const list = document.getElementById("list")
+const error = document.getElementById("error")
 
 button.addEventListener('click', () => {
-    if (!input.value.trim()) return
+    const value =input.value.trim()
+     if (value === "") {
+        error.textContent = "Task cannot be empty"
+        return
+    }
+    error.textContent=""
 
     const li = document.createElement('li')
     const text = document.createElement('span')
     const edit = document.createElement('button')
     const remove = document.createElement('button')
-    text.textContent = input.value
+    text.textContent = value
     edit.textContent = "edit"
     remove.textContent = "delete"
 
     edit.addEventListener('click',()=>{
         if(edit.textContent==='edit'){
             const newText =prompt('Edit Task',text.textContent)
-            if(newText){
-                text.textContent=newText
+            if(newText && newText.trim()!==""){
+                text.textContent=newText.trim()
             }
         }
     })
