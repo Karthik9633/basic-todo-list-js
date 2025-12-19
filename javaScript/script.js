a0add87280fcc85c870f99f3193a30c107ad6a65
Karthik B <karthikbalu5533@gmail.com>
2025-12-19 14:05:03 +0530

    javascript is completed

---
+++ javaScript/script.js
@@ -0,0 +1,29 @@
+const input = document.getElementById("taskInput")
+const button = document.getElementById("btnClick")
+const list = document.getElementById("list")
+
+button.addEventListener('click', () => {
+    if (!input.value.trim()) return
+
+    const li = document.createElement('li')
+    const text = document.createElement('p')
+    const edit = document.createElement('button')
+    const remove = document.createElement('button')
+    text.textContent = input.value
+    edit.textContent = "edit"
+    remove.textContent = "remove"
+
+    edit.addEventListener('click',()=>{
+        if(edit.textContent==='edit'){
+            const newText =prompt('Edit Task',text.textContent)
+            if(newText){
+                text.textContent=newText
+            }
+        }
+    })
+    remove.addEventListener('click',()=> li.remove())
+
+    li.append(text,edit,remove)
+    list.appendChild(li)
+    input.value=""
+})
