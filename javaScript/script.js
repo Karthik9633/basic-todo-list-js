fefa055ef9827fbcc24640fd092650e44dbe617d
Karthik B <karthikbalu5533@gmail.com>
2025-12-20 14:10:28 +0530

    add and indisplay input box for edit and get rid of the prompt function

--- javaScript/script.js
+++ javaScript/script.js
@@ -4,32 +4,42 @@ const list = document.getElementById("list")
 const error = document.getElementById("error")
 
 button.addEventListener('click', () => {
-    const value =input.value.trim()
-     if (value === "") {
+    const value = input.value.trim()
+    if (value === "") {
         error.textContent = "Task cannot be empty"
         return
     }
-    error.textContent=""
+    error.textContent = ""
 
     const li = document.createElement('li')
     const text = document.createElement('span')
     const edit = document.createElement('button')
     const remove = document.createElement('button')
     text.textContent = value
-    edit.textContent = "edit"
-    remove.textContent = "delete"
+    edit.textContent = "Edit"
+    remove.textContent = "Delete"
 
-    edit.addEventListener('click',()=>{
-        if(edit.textContent==='edit'){
-            const newText =prompt('Edit Task',text.textContent)
-            if(newText && newText.trim()!==""){
-                text.textContent=newText.trim()
+    edit.addEventListener('click', () => {
+        if (edit.textContent === "Edit") {
+            text.contentEditable = true
+            text.focus()
+            edit.textContent = "Save"
+        } else {
+            if (text.textContent.trim() === "") {
+                error.textContent = "Task cannot be empty"
+                text.focus()
+                return
             }
+
+            error.textContent = ""
+            text.textContent = text.textContent.trim()
+            text.contentEditable = false
+            edit.textContent = "Edit"
         }
     })
-    remove.addEventListener('click',()=> li.remove())
+    remove.addEventListener('click', () => li.remove())
 
-    li.append(text,edit,remove)
+    li.append(text, edit, remove)
     list.appendChild(li)
-    input.value=""
+    input.value = ""
 })
